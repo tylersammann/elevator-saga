8cd114c972934d53ed7d0b3f0301f6dba55f301d
Tyler Sammann <TSAMMAN@TSAMMAN-MBP.local>
2015-01-31 00:51:26 -0600

    got past 10

--- solution.js
+++ solution.js
@@ -5,18 +5,50 @@
             var e = elevators[i];
 
             e.on("idle", function() {
+                // e.goToFloor(0);
             });
 
             e.on("floor_button_pressed", function(floorNum) {
-                if (e.loadFactor() > 0.5) {
+                if (e.loadFactor() > 0.3) {
                     e.goToFloor(floorNum);
                 }
-            })
-
+            });
         }
 
-        for(var i = 0; i < elevators.length; i++) {
+        function bestElevatorGoToFloor(floorNum) {
+            var shortestDistance = 1000000;
+            var bestElevator = 0;
+            for (var i=0; i<elevators.length; i++) {
+                var currentDistance = Math.abs(elevators[i].currentFloor() - floors[floorNum].floorNum());
+                if (currentDistance < shortestDistance) {
+                    shortestDistance = currentDistance;
+                    bestElevator = i;
+                }
+            }
+            // return i;
+            // if (elevators[bestElevator].loadFactor() < 0.3) {
+                elevators[bestElevator].goToFloor(floorNum);
+            // }
+        }
+
+        function setFloorCallbacks(j) {
+            var f = floors[j];
+
+            f.on("up_button_pressed", function() {
+                bestElevatorGoToFloor(j);
+            });
+
+            f.on("down_button_pressed", function() {
+                bestElevatorGoToFloor(j);
+            });
+        }
+
+        for (var i = 0; i < elevators.length; i++) {
             setElevatorCallbacks(i);
+        }
+
+        for (var j = 0; j < floors.length; j++) {
+            setFloorCallbacks(j);
         }
 
     },
